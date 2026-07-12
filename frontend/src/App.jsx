291976df67bb5d002b820e390cd57ba6417e5811
Sumith Manala <sumith75@gmail.com>
2026-07-12 10:48:34 +0530

    project initialisation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-600">
+      <h1 className="text-5xl font-extralight text-black-100">
+        Tailwind v4 Working 🚀
+      </h1>
+    </div>
+  );
+}
